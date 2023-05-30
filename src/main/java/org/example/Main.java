e04db22ad1517cfc2ed9fc2eb6f19298483338e2
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2023-05-30 15:05:34 -0400

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    UserInterface ui= new UserInterface();
+    ui.display();
+
+
     }
+
 }
