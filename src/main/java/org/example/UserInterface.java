5665cc7990baa2455229502bc5e99fc38759bb43
Masuma-N <113317957+Masuma-N@users.noreply.github.com>
2023-05-30 15:12:53 -0400

    initial commit

---
+++ src/main/java/org/example/UserInterface.java
@@ -0,0 +1,40 @@
+package org.example;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+
+public class UserInterface {
+    public static Scanner scanner= new Scanner(System.in);
+    public void display(){
+        displayHome();
+    }
+    private void displayHome() {
+
+
+        System.out.println("Welcome to our Deli/shop! How may I help you? ");
+
+        System.out.println("\n \n Our Menu: ");
+        //inputs will display the menu
+        System.out.println("New Order [1]");
+        System.out.println("Exit [2]");
+        String Userinput = scanner.nextLine();
+
+        switch(Userinput){
+            case "1" ->startOrder();
+            case "2" -> {
+                System.out.println("Goodbye, have a nice day!");
+                System.exit(0);
+            }
+            default -> System.out.println();
+
+        }
+
+
+
+    }
+
+    private void startOrder() {
+    }
+
+
+}
