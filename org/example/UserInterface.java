2b7f4aa7a46f625c18eef88d91d43a5eb8de3cc1
grgkarma45 <grgkarma45@gmail.com>
2023-05-31 15:35:10 -0400

    initial commit

--- org/example/UserInterface.java
+++ org/example/UserInterface.java
@@ -9,6 +9,7 @@ public class UserInterface {
     private static final double[] SANDWICH_SIZE_PRICES = {5.50, 7.00, 8.50}; // Prices for each sandwich size
     private static List<String> orderEntries = new ArrayList<>(); // List to store order entries
     private static Scanner scanner = new Scanner(System.in);
+    private static double totalPrice = 0.0;
 
     public static void main(String[] args) {
         displayHome();
@@ -110,7 +111,6 @@ public class UserInterface {
         BreadChoice selectedBread = BreadChoice.values()[breadChoiceIndex - 1];
         double breadPrice = selectedBread.getPrice();
 
-
         // Prompt for meat selection
         System.out.println("Select the meats:");
         for (MeatChoice meatChoice : MeatChoice.values()) {
@@ -137,18 +137,23 @@ public class UserInterface {
         System.out.println("Sandwich price: $" + sandwichPrice);
         System.out.println("Selected bread: " + selectedBread.getDisplayName());
         System.out.println("Bread price: $" + breadPrice);
-
         System.out.println("Selected meats: " + meats);
         System.out.println("Meat price: $" + meatPrice);
+
+        double sandwichTotalPrice = sandwichPrice + breadPrice + meatPrice;
+        orderEntries.add("Sandwich - Size: " + sandwichSize + "\" - $" + sandwichTotalPrice);
+        totalPrice += sandwichTotalPrice;
+
+        System.out.println("Sandwich added to the order.");
     }
 
     private static double getMeatPriceForSize(double meatPrice, double sandwichSize) {
         if (sandwichSize == 4) {
-            return meatPrice * 1.00;  // Meat price for 4-inch sandwich
+            return meatPrice;  // Meat price for 4-inch sandwich
         } else if (sandwichSize == 8) {
-            return meatPrice * 2.00;  // Meat price for 8-inch sandwich
+            return meatPrice + 1;  // Meat price for 8-inch sandwich
         } else if (sandwichSize == 12) {
-            return meatPrice * 3.00;  // Meat price for 12-inch sandwich
+            return meatPrice + 2;  // Meat price for 12-inch sandwich
         } else {
             System.out.println("Invalid sandwich size. Default meat price will be used.");
             return meatPrice * 1.00;  // Default meat price
@@ -156,15 +161,50 @@ public class UserInterface {
     }
 
     private static void addDrink() {
-        // TODO: Implement adding a drink to the order
+        System.out.println("Adding a drink to the order...");
+
+        System.out.println("Select the drink size:");
+        for (DrinkChoice drinkChoice : DrinkChoice.values()) {
+            System.out.println(drinkChoice.ordinal() + 1 + ") " + drinkChoice.getSize() + " - $" + drinkChoice.getPrice());
+        }
+
+        int sizeChoice = scanner.nextInt();
+        if (sizeChoice < 1 || sizeChoice > DrinkChoice.values().length) {
+            System.out.println("Invalid choice. Cancelling drink addition.");
+            return;
+        }
+
+        DrinkChoice selectedDrink = DrinkChoice.values()[sizeChoice - 1];
+        double drinkPrice = selectedDrink.getPrice();
+
+        orderEntries.add("Drink - " + selectedDrink.getSize() + " - $" + drinkPrice);
+        totalPrice += drinkPrice;
+
+        System.out.println("Selected drink: " + selectedDrink.getSize() + " - $" + drinkPrice);
+        System.out.println("Drink added to the order.");
     }
 
     private static void addChips() {
-        // TODO: Implement adding chips to the order
+        System.out.println("Adding chips to the order...");
+        System.out.println("Do you want to add chips? (Y/N)");
+
+        String choice = scanner.next();
+        if (choice.equalsIgnoreCase("Y")) {
+            orderEntries.add("Chips - $1.50");
+            totalPrice += 1.50;
+            System.out.println("Chips added to the order.");
+            System.out.println("It will be an extra $1.50.");
+        } else {
+            System.out.println("Chips not added to the order.");
+        }
     }
 
     private static void checkout() {
-        // TODO: Implement the order checkout
+        System.out.println("Order Summary:");
+        for (String entry : orderEntries) {
+            System.out.println(entry);
+        }
+        System.out.println("Total Price: $" + totalPrice);
     }
 
     private static void cancelOrder() {
