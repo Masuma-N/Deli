9f239f3fe9dffe8f5f996bfe393d0d45e1cdab2b
m-miron <mironmaricarmen@gmail.com>
2023-05-30 21:35:56 -0400

    Modified.

--- src/main/java/org/example/UserInterface.java
+++ src/main/java/org/example/UserInterface.java
@@ -1,39 +1,37 @@
 package org.example;
-import java.sql.SQLOutput;
+
 import java.util.Scanner;
 
-
 public class UserInterface {
-    public static Scanner scanner= new Scanner(System.in);
-    public void display(){
+    public static Scanner scanner = new Scanner(System.in);
+
+    public void display() {
         displayHome();
     }
+
     private void displayHome() {
-
-
-        System.out.println("Welcome to our Deli/shop! How may I help you? ");
-
-        System.out.println("\n \n Our Menu: ");
         //inputs will display the menu
-        System.out.println("New Order [1]");
-        System.out.println("Exit [2]");
+        System.out.println("""
+                Welcome to our Deli!\s
+                                
+                How may I help you?
+                [1] Start New Order
+                [2] Exit\s
+                """);
         String Userinput = scanner.nextLine();
 
-        switch(Userinput){
-            case "1" ->startOrder();
+        switch (Userinput) {
+            case "1" -> startOrder();
             case "2" -> {
                 System.out.println("Goodbye, have a nice day!");
                 System.exit(0);
             }
             default -> System.out.println();
-
         }
-
-
-
     }
 
     private void startOrder() {
+        System.out.println("Sorry! Nothing here yet.");
     }
 
 
