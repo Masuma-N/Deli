c036f31d0b03ed8172933d9f6d6b43e234c87c5f
m-miron <mironmaricarmen@gmail.com>
2023-06-01 22:55:13 -0400

    Totalprice calculated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-package src.main.java.org.example;
+package org.example;
 
 public class Main {
     public static void main(String[] args) {
--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,24 +3,24 @@ package org.example;
 import static org.example.UserInterface.scanner;
 
 public class Order {
-private void displayOrder(){
-    System.out.println("Order Screen");
-    System.out.println("1) Add Sandwich");
-    System.out.println("2) Add Drink");
-    System.out.println("3) Add Chips");
-    System.out.println("4) Checkout");
-    System.out.println("0) Cancel Order");
-    String Userinput = scanner.nextLine();
+    private void displayOrder(){
+        System.out.println("Order Screen");
+        System.out.println("1) Add Sandwich");
+        System.out.println("2) Add Drink");
+        System.out.println("3) Add Chips");
+        System.out.println("4) Checkout");
+        System.out.println("0) Cancel Order");
+        String Userinput = scanner.nextLine();
 
-    switch(Userinput){
-        case "1"-> addSandwich();
-        case "2" ->addDrink();
-        case "3"->addChips();
-        case "4" ->checkout();
-        case "0" ->cancelOrder();
-        default -> System.out.println("Invalid choice. Please try again.");
+        switch(Userinput){
+            case "1"-> addSandwich();
+            case "2" ->addDrink();
+            case "3"->addChips();
+            case "4" ->checkout();
+            case "0" ->cancelOrder();
+            default -> System.out.println("Invalid choice. Please try again.");
 
-    }
+        }
     }
 
     private void cancelOrder() {
